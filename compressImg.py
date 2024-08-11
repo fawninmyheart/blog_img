e6e9fda27e217381cf7e9c1c98aabfdeb0c5614f
沈华 <shenhua@shenhuadeMacBook-Air.local>
2024-08-12 00:22:18 +0800

    新增了素描历史

--- compressImg.py
+++ compressImg.py
@@ -1,6 +1,8 @@
-import os
+import os,sys
 from PIL import Image
-import sys
+from pillow_heif import register_heif_opener
+
+register_heif_opener()
 
 def is_image(filepath):
     try:
@@ -9,7 +11,8 @@ def is_image(filepath):
         return True
     except:
         return False
-    
+
+h2p_compress_ratio = 2
 in_path=sys.argv[1].rstrip('/')
 img_file_list=os.listdir(in_path)
 
@@ -22,12 +25,17 @@ for i in img_file_list:
         if os.path.isfile(in_path+'/'+imgID+"_small."+suffix):
             continue
         img=Image.open(in_path+'/'+i)
+        # if suffix.lower() == "heic":
+        #     suffix = "png"
+        #     img_raw=img.resize((int(img.size[0]/h2p_compress_ratio),int(img.size[1]/h2p_compress_ratio))).convert('sRGB')
+        #     img_raw.save("{imgPath}/{imgID}.h2p.{suffix}".format(imgPath = in_path, imgID = imgID, suffix = suffix))
         if img.size[0]>img.size[1]:
             width = 896
         else:
             width =  448
         if(img.size[0]>width):
             height = int(width*img.size[1]/img.size[0])
-            img=img.resize((width,height))
+            img=img.resize((width,height))#.convert('sRGB')
 
         img.save("{imgPath}/{imgID}_small.{suffix}".format(imgPath = in_path, imgID = imgID, suffix = suffix))
+        # break
